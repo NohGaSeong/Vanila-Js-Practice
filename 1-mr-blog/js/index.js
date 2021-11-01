4244d8443bb6c7a60da90c64e0733a18e6d812bc
노가성 <82383294+NohGaSeong@users.noreply.github.com>
2021-11-01 23:26:10 +0900

    Add files via upload

--- 1-mr-blog/js/index.js
+++ 1-mr-blog/js/index.js
@@ -1,91 +1,83 @@
-let date = new Date();
-let currentMonth = date.getMonth()+1;
-let currentDate = date.getUTCDate();
-let list = [31,28,31,30,31,30,31,31,30,31,30,31];
-let current = String(currentMonth)+currentDate;
-let question;
-let dataArr;
 
-let prevBtn = document.querySelector("#prev-btn");
-let nextBtn = document.querySelector("#next-btn");
-let saveBtn = document.querySelector("#save-btn");
+let answer = document.getElementById("answer-input");
+let question = document.getElementById("question");
+let i = 0;
+let now = new Date();
+let month = now.getMonth() + 1;
+let date = now.getDate();
+let year = now.getFullYear();
 
-prevBtn.addEventListener("click", handlePrev);
-nextBtn.addEventListener("click", handleNext);
-saveBtn.addEventListener("click", handleSave);
-
-import {getAnswer,setAnswer } from './utils/storage.js';
-
+document.getElementById("prev-btn").addEventListener("click", handlePrev);
+document.getElementById("next-btn").addEventListener("click", handleNext);
+document.getElementById("save-btn").addEventListener("click", handleSave);
 
 /**
  * 저장 버튼 클릭 함수
  */
-function handleSave() { 
-    setAnswer(answerkey,answerInput.value);
+function handleSave() {
+    localStorage.setItem(String(month) + String(date), answer.value);
 }
 
+function look() {
+    answer. value= localStorage.getItem(String(month) + String(date));
+}
 /**
  * 오른쪽 화살표 버튼 클릭 함수
  */
 function handleNext() {
-    if (currentDate == list[currentMonth-1]) {
-        currentMonth++;
-        currentDate=1;
-    } else {
-        currentDate++;
+    date = date + 1;
+    if (date === new Date(year, month, 0).getDate() + 1) {
+        date = 1;
+        if (month === 12) {
+            month = 0;
+        }
+        month = month + 1;
     }
-    checkDate(currentMonth,currentDate);
-    printDate();
+    document.getElementById("month").innerText = month;
+    document.getElementById("day").innerText = date;
+    look();
+    fetchData();
 }
 
 /**
  * 왼쪽 화살표 버튼 클릭 함수
  */
 function handlePrev() {
-    if (currentDate == 1) {
-        currentMonth--;
-        currentDate=list[currentMonth-1];
-    } else {
-        currentDate--;
+    date = date - 1;
+    if (date === 0) {
+        month = month - 1;
+        date = new Date(year, month, 0).getDate();
+        if (month === 0) {
+            month = 12;
+        }
     }
-    checkDate(currentMonth,currentDate);
-    printDate();
+    document.getElementById("month").innerText = month;
+    document.getElementById("day").innerText = date;
+    look();
+    fetchData();
+}
+
+function fetchData(){
+    fetch("./db/data.json")
+    .then(function (resp) {
+        return resp.json();
+    })
+    .then(function (data) {
+        for (let i = 0; i < data.questions.length; i++) {
+            if (String(month) + String(date) === data.questions[i].id) {
+                question.innerText = data.questions[i].question;
+            }
+        }
+    })
 }
 
 /**
  * 서비스 초기화 함수
  */
 function init() {
-    fetch("../db/data.json")
-    .then(response => response.json())
-    .then(datas =>{ 
-        datas = datas.questions;
-        dataArr = datas;
-        printDate();
-        });
-    }
-
-
-function printDate() {
-    dataArr.forEach(data => {
-        if (data.id === current) {
-            question = data.question;
-        }
-        document.querySelector("#month").innerText = currentMonth;
-        document.querySelector("#day").innerText = currentDate;
-        document.querySelector("#question").innerText = question;
-    });
+    fetchData();
+    document.getElementById("month").innerText = month;
+    document.getElementById("day").innerText = date;
+   
 }
-
-function checkDate(currentMonth,currentDate) {
-    if (currentMonth<10) {
-        currentMonth = "0"+currentMonth;
-    }
-    if (currentDate<10) {
-        currentDate = "0"+currentDate;
-    }
-    current = String(currentMonth)+currentDate;
-    console.log(current)
-}
-
 init();
